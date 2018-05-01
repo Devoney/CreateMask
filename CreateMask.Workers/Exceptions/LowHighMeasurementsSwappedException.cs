02d8ce9d7188dd46c0184ed6374f21ebbce609d6
Mike de Klerk <mikedeklerk@gmail.com>
2018-05-01 18:13:36 +0200

    26 exception for swapped low high measurements csv (#32)
    * https://github.com/Devoney/CreateMask/issues/26
    1.) Adds exception types and implements logic to detect whether there are low measurements >= high measurements.
    Or that all low measurements are higher or == to low measurements, probably meaning that the files have been swapped.
    Not tested yet.
    2.) Fixes wrongly implemented logic of resistance comparison and exception throwing.
    3..) Adds unit test to check if all measurements from low light intensity actually show brighter light intensity than the high light intensity measurements.
    4.) Adds unit test to check if the statement above is true for a subset of measurements.
    5.) Changes the labels in the UI to better describe what needs to be filled in for the low/high light intensity measurements csv files.

---
+++ CreateMask.Workers/Exceptions/LowHighMeasurementsSwappedException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+
+namespace CreateMask.Workers.Exceptions
+{
+    [Serializable]
+    public class LowHighMeasurementsSwappedException : Exception
+    {
+        public LowHighMeasurementsSwappedException()
+        {
+        }
+
+        public LowHighMeasurementsSwappedException(string message)
+            : base(message)
+        {
+        }
+
+        public LowHighMeasurementsSwappedException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected LowHighMeasurementsSwappedException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            
+        }
+
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+                throw new ArgumentNullException(nameof(info));
+            base.GetObjectData(info, context);
+        }
+    }
+}
