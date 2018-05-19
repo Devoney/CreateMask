bddc48fb8ee2f8cca155d5f9337bba7c7cb7eb1a
MikeDeKlerk <mikedeklerk@gmail.com>
2018-05-19 22:49:19 +0200

    https://github.com/Devoney/CreateMask/issues/30 - Makes unit test WhenFileDoesNotExistItsContentsIsNull work - Removes tests that have become redundant.

--- CreateMask.Workers.Test/ErrorReportCreatorTests.cs
+++ CreateMask.Workers.Test/ErrorReportCreatorTests.cs
@@ -5,6 +5,7 @@ using CreateMask.Containers;
 using CreateMask.Contracts.Interfaces;
 using FluentAssertions;
 using Moq;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using TestHelpers;
 
@@ -87,28 +88,62 @@ namespace CreateMask.Workers.Test
         [TestCase(nameof(ApplicationArguments.LdrCalibrationFilePath), nameof(ErrorReport.LdrCurveCsvData))]
         [TestCase(nameof(ApplicationArguments.LcdMeasurementsFilePathLow), nameof(ErrorReport.MeasurementsLowCsvData))]
         [TestCase(nameof(ApplicationArguments.LcdMeasurementsFilePathHigh), nameof(ErrorReport.MeasurementsHighCsvData))]
-        public void FileContentsIsAddedToErrorReport(string applicationArgumentsPropertyName, string errorReportPropertyName)
-        {
-            //Given
-
-            //When
-
-            //Then
-            Assert.Fail("todo");
-        }
-
-        [Test, Category(Categories.Unit)]
-        [TestCase(nameof(ApplicationArguments.LdrCalibrationFilePath), nameof(ErrorReport.LdrCurveCsvData))]
-        [TestCase(nameof(ApplicationArguments.LcdMeasurementsFilePathLow), nameof(ErrorReport.MeasurementsLowCsvData))]
-        [TestCase(nameof(ApplicationArguments.LcdMeasurementsFilePathHigh), nameof(ErrorReport.MeasurementsHighCsvData))]
         public void WhenFileDoesNotExistItsContentsIsNull(string applicationArgumentsPropertyName, string errorReportPropertyName)
         {
-            //Given
+            StorageManager.InTemporaryDirectory(directory =>
+            {
+                using (var tempFileManager = new TempFileManager(directory))
+                {
+                    //Given
+                    const string reportName = "JEU76-12-95FE";
+                    var errorReportCreator = GetErrorReportCreator();
+                    const string exceptionMessage = reportName + " exception message";
+                    var exception = new Exception(exceptionMessage);
+                    var version = new Version(1, 2, 3, 4);
+                    var applicationArguments = new ApplicationArguments();
 
-            //When
+                    const string ldrContents = "Ldr file contents";
+                    const string highContents = "High file contents";
+                    const string lowContents = "Low file contents";
+                    applicationArguments.DesiredResistance = 8820;
+                    applicationArguments.FileType = "bmp";
+                    applicationArguments.High = 20;
+                    applicationArguments.Low = 128;
+                    applicationArguments.LcdHeight = 1440;
+                    applicationArguments.LcdWidth = 2560;
+                    applicationArguments.MaskFilePath = @"c:\user\my\dir\mask.bmp";
+                    applicationArguments.OriginalExposureTime = 1540;
+                    applicationArguments.MeasurementsNrOfColumns = 7;
+                    applicationArguments.MeasurementsNrOfRows = 9;
+                    if (applicationArgumentsPropertyName != nameof(ApplicationArguments.LdrCalibrationFilePath))
+                    {
+                        applicationArguments.LdrCalibrationFilePath = tempFileManager.GetTempFile(ldrContents);
+                    }
+                    if (applicationArgumentsPropertyName != nameof(ApplicationArguments.LcdMeasurementsFilePathHigh))
+                    {
+                        applicationArguments.LcdMeasurementsFilePathHigh = tempFileManager.GetTempFile(highContents);
+                    }
+                    if (applicationArgumentsPropertyName != nameof(ApplicationArguments.LcdMeasurementsFilePathLow))
+                    {
+                        applicationArguments.LcdMeasurementsFilePathLow = tempFileManager.GetTempFile(lowContents);
+                    }
 
-            //Then
-            Assert.Fail("todo");
+                    //When
+                    errorReportCreator.CreateReport(version, exception, applicationArguments, directory, reportName);
+
+                    //Then
+                    var filesInDirectory = Directory.GetFiles(directory, reportName + "*").ToList();
+                    filesInDirectory.Count.Should().Be(1);
+                    var report = filesInDirectory.Single();
+
+                    var actualFileContents = File.ReadAllText(report);
+                    var actualErrorReport = JsonConvert.DeserializeObject<ErrorReport>(actualFileContents);
+
+                    var propertyInfo = typeof (ErrorReport).GetProperty(errorReportPropertyName);
+                    var propertyValue = propertyInfo.GetValue(actualErrorReport);
+                    propertyValue.Should().BeNull();
+                }
+            });
         }
 
         private IErrorReportCreator GetErrorReportCreator()
