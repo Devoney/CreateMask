0b1965d54a48578cbd0ec8e39330e58fd566f0c4
Mike de Klerk <mikedeklerk@gmail.com>
2018-05-27 13:29:40 +0200

    https://github.com/Devoney/CreateMask/issues/28 Adds unit test to make sure that directory is created where reported error reports are moved to.

---
+++ CreateMask.Workers.Test/ErrorReportReporterTests.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using CreateMask.Containers;
+using CreateMask.Contracts.Interfaces;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using TestHelpers;
+
+namespace CreateMask.Workers.Test
+{
+    [TestFixture]
+    public class ErrorReportReporterTests
+    {
+        [Test, Category(Categories.Unit)]
+        public void DirectoryToMoveReportedReportsToIsCreated()
+        {
+            StorageManager.InTemporaryDirectory(directory =>
+            {
+                //Given
+                const string subDirName = "reported";
+                var subDir = Path.Combine(directory, subDirName);
+                var errorReportConfiguration = new ErrorReportConfiguration(directory, subDir);
+                var container = GetErrorReportReporter(errorReportConfiguration);
+                var errorReportReporter = container.ErrorReportReporter;
+
+                //When
+                errorReportReporter.Start();
+
+                //Then
+                Directory.Exists(subDir).Should().BeTrue();
+            });
+        }
+
+        private static GetContainer GetErrorReportReporter(ErrorReportConfiguration errorReportConfiguration)
+        {
+            var fileSystemWatcherMock = new Mock<IFileSystemWatcher>();
+            var gitHubIssueCreatorMock = new Mock<IGitHubIssueCreator>();
+            var errorReportReporter = new ErrorReportReporter(fileSystemWatcherMock.Object, errorReportConfiguration, gitHubIssueCreatorMock.Object);
+            return new GetContainer(fileSystemWatcherMock, errorReportReporter, errorReportConfiguration);
+        }
+
+        private class GetContainer
+        {
+            public Mock<IFileSystemWatcher> FileSystemWatcherMock { get; private set; }
+            public ErrorReportReporter ErrorReportReporter { get; private set; }
+            public ErrorReportConfiguration ErrorReportConfiguration { get; private set; }
+
+            public GetContainer(Mock<IFileSystemWatcher> fileSystemWatcherMock, ErrorReportReporter errorReportReporter, ErrorReportConfiguration errorReportConfiguration)
+            {
+                FileSystemWatcherMock = fileSystemWatcherMock;
+                ErrorReportReporter = errorReportReporter;
+                ErrorReportConfiguration = errorReportConfiguration;
+            }
+        }
+    }
+}
