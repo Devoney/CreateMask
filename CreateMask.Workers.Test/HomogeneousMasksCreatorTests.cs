e61d51483ef1efd4ce464581de1010997a03203e
MikeDeKlerk <mikedeklerk@gmail.com>
2018-05-30 20:58:40 +0200

    https://github.com/Devoney/CreateMask/issues/7 Adds unit test to verify that correct number of masks are created.

--- CreateMask.Workers.Test/HomogeneousMasksCreatorTests.cs
+++ CreateMask.Workers.Test/HomogeneousMasksCreatorTests.cs
@@ -1,4 +1,6 @@
 ﻿using System.Drawing;
+using System.Linq;
+using CreateMask.Contracts.Enums;
 using FluentAssertions;
 using NUnit.Framework;
 using TestHelpers;
@@ -16,16 +18,30 @@ namespace CreateMask.Workers.Test
             var size = new Size(32, 48);
             var color = Color.FromArgb(56, 56, 56);
             var filePath = StorageManager.GetFullFilePath("homogeneousmask32x48-56.bmp");
-            using (Bitmap expectedBitmap = (Bitmap)Image.FromFile(filePath))
-            {
+            var expectedBitmap = Image.FromFile(filePath);
 
-                //When
-                using (var bitmap = hmc.CreateMask(size, color))
-                {
-                    //Then
-                    AssertExt.Equals(expectedBitmap, bitmap);
-                }
-            }
+            //When
+            var bitmap = hmc.CreateMask(size, color);
+
+            //Then
+            AssertExt.Equals(expectedBitmap, bitmap);
+        }
+
+        [Test, Category(Categories.Unit)]
+        [TestCase(IntensityDifference.Id1, 256)]
+        [TestCase(IntensityDifference.Id8, 32)]
+        [TestCase(IntensityDifference.Id64, 4)]
+        public void CorrectNumberOfMasksIsCreated(IntensityDifference intensityDifference, int expectedNrOfMasks)
+        {
+            //Given
+            var hmc = GetHomogeneousMasksCreator();
+            var size = new Size(1, 1);
+
+            //When
+            var masks = hmc.CreateSetOfMasks(size, intensityDifference);
+
+            //Then
+            masks.Count().Equals(expectedNrOfMasks);
         }
 
         private IHomogeneousMasksCreator GetHomogeneousMasksCreator()
